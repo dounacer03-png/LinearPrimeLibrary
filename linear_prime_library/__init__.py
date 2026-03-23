d1fcb88fd1d01f7e28e7c6c6cfc6536ede069d8a
dounacer03-png <dounacer03@gmail.com>
2026-03-23 11:34:13 +0100

    Create __init__.py
    """
    LinearPrimeLibrary - مكتبة المعادلات الخطية
    """
    
    __version__ = "1.0.0"
    __author__ = "Dou Abdennasser"
    
    from .core import (
        LinearPrimeLibrary,
        LinearPrimeCrypto,
        EquationInfo,
        Cycle
    )
    
    __all__ = [
        'LinearPrimeLibrary',
        'LinearPrimeCrypto',
        'EquationInfo',
        'Cycle'
    ]

---
+++ linear_prime_library/__init__.py
@@ -0,0 +1,20 @@
+"""
+LinearPrimeLibrary - مكتبة المعادلات الخطية
+"""
+
+__version__ = "1.0.0"
+__author__ = "Dou Abdennasser"
+
+from .core import (
+    LinearPrimeLibrary,
+    LinearPrimeCrypto,
+    EquationInfo,
+    Cycle
+)
+
+__all__ = [
+    'LinearPrimeLibrary',
+    'LinearPrimeCrypto',
+    'EquationInfo',
+    'Cycle'
+]
